15122635b23f1c8a2e6e2fad460b88f7f1622cf1
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-03-05 01:03:52 -0300

    feat(doctor/compoentns): deteUpdate added

---
+++ src/app/doctor/components/date-update/date-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-update',
+  templateUrl: './date-update.component.html',
+  styleUrls: ['./date-update.component.less']
+})
+export class DateUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
