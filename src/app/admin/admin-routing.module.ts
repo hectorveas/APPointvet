37285faf5bd8d019ff427995cc2378dbcdece2a4
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-03-05 19:31:55 -0300

    feat(admin): routes fixed

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeScreenComponent } from './screens/home-screen/admin-home-screen.component';
+import { AdminLoginScreenComponent } from './screens/login-screen/admin-login-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminLoginScreenComponent,
+    pathMatch: 'full'
+  },
+  { path: 'home', component: AdminHomeScreenComponent },
+  {
+    path: '**',
+    component: AdminLoginScreenComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
