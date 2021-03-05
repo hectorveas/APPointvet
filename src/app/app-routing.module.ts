07130f0d9b9f0f0675de854adc95e99524f56944
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-03-05 01:07:58 -0300

    feat(app): routing app added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'visitor/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'doctor',
+    loadChildren: () => import('./doctor/doctor.module')
+      .then(DoctorModule => DoctorModule.DoctorModule)
+  },
+  {
+    path: 'paciente',
+    loadChildren: () => import('./patient/patient.module')
+      .then(PatientModule => PatientModule.PatientModule)
+  },
+  {
+    path: 'visitante',
+    loadChildren: () => import('./visitor/visitor.module')
+      .then(VisitorModule => VisitorModule.VisitorModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module')
+      .then(AdminModule => AdminModule.AdminModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'visitor/inicio',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
