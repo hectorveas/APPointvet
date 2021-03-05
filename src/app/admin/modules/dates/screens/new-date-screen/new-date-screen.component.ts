0707bc9518e296cbacf879cbdcbc6b4e45740995
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-03-05 19:28:55 -0300

    feat(admin/modules/dates): screens added

---
+++ src/app/admin/modules/dates/screens/new-date-screen/new-date-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-date-screen',
+  templateUrl: './new-date-screen.component.html',
+  styleUrls: ['./new-date-screen.component.less']
+})
+export class NewDateScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/admin/modules/dates/screens/update-date-screen/update-date-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-date-screen',
+  templateUrl: './update-date-screen.component.html',
+  styleUrls: ['./update-date-screen.component.less']
+})
+export class UpdateDateScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
