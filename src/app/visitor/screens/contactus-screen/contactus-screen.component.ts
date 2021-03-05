6494b43e6a9484a53922b8e246ab2330a0f94964
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-03-05 01:07:20 -0300

    feat(visitor): screens added

---
+++ src/app/visitor/screens/contactus-screen/contactus-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'visitor-contactus-screen',
+  templateUrl: './contactus-screen.component.html',
+  styleUrls: ['./contactus-screen.component.less']
+})
+export class ContactusScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/visitor/screens/login-screen/login-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'visitor-login-screen',
+  templateUrl: './login-screen.component.html',
+  styleUrls: ['./login-screen.component.less']
+})
+export class LoginScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
