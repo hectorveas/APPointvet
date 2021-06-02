83821744faf2d6073cf121f33cb44e18e2245823
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-01 22:18:17 -0400

    fix(app): routes of home fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from '@visitor/screens/home-screen/home-screen.component';
 
 const routes: Routes = [
-
   {
     path: '',
-    redirectTo: 'visitor/inicio',
+    component: HomeScreenComponent,
     pathMatch: 'full'
+  },
+  {
+    path: 'visitor',
+    loadChildren: () => import('./visitor/visitor.module')
+      .then(VisitorModule => VisitorModule.VisitorModule)
   },
   {
     path: 'doctor',
@@ -19,18 +24,13 @@ const routes: Routes = [
       .then(PatientModule => PatientModule.PatientModule)
   },
   {
-    path: 'visitante',
-    loadChildren: () => import('./visitor/visitor.module')
-      .then(VisitorModule => VisitorModule.VisitorModule)
-  },
-  {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module')
       .then(AdminModule => AdminModule.AdminModule)
   },
   {
     path: '**',
-    redirectTo: 'visitor/inicio',
+    component: HomeScreenComponent
   },
 ];
 
