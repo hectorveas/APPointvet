d88801dd8c31ea955cc0f6b70f5a2e7c49ca79e1
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-06 19:52:26 -0400

    feat(core): httpservices updated

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -1,10 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { map } from "rxjs/operators";
 
+interface HttpOptions {
+  headers: HttpHeaders
+};
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  // Incorporar manejo de errores y manejo de Sentry
-  constructor() { }
+  private httpOptions: HttpOptions;
+  private baseUrl: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = environment.apiBaseUrl;
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': environment.AUTH_KEY
+      })
+    };
+  }
+
+  public get<type>(path: string){
+    return this.httpClient.get<type>(this.baseUrl + path, this.httpOptions)
+      .pipe(
+        map((data: any) => data as type)
+      );
+  }
+
+  public post<type>(path: string, body: any){
+    return this.httpClient.post<type>(this.baseUrl + path, body, this.httpOptions);
+  }
+
+  public patch<type>(path: string, body: any) {
+    return this.httpClient.patch<type>(this.baseUrl + path, body, this.httpOptions);
+  }
+
+  public delete<type>(path: string){
+    return this.httpClient.delete<type>(this.baseUrl + path, this.httpOptions);
+  }
 }
