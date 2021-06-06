1136dc5cdc4bf465c13b88feb251aa46dcbecd38
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-06 19:52:12 -0400

    feat(core): authprovider added

--- src/app/core/providers/auth/auth-provider.service.ts
+++ src/app/core/providers/auth/auth-provider.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Doctor } from '@core/models/doctor.model';
+import { Paciente } from '@core/models/paciente.model';
+import { HttpService } from '@core/services/http/http.service';
+import { TokenService } from '@core/services/token/token.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthProviderService {
 
-  constructor() { }
+  private authenticated: boolean;
+  private currentUser: any;
+
+  constructor(private httpService: HttpService, private tokenService: TokenService, private router: Router) {
+    this.authenticated = false;
+    this.currentUser = null;
+
+    if (sessionStorage.getItem('credentials')) {
+      this.authenticated = true;
+    }
+  }
+
+  public isAuthenticated(): boolean {
+    return this.authenticated;
+  }
+
+  public async getCurrentUser(): Promise<Paciente | Doctor>{
+    if (!this.currentUser){
+      this.currentUser = await this.httpService.get<Paciente | Doctor>('').toPromise();
+    }
+    return this.currentUser;
+  }
+
+  public login(auth: { email: string, password: string }): Observable<any> {
+    return this.httpService.post<any>('/login', {
+      email: auth.email,
+      password: auth.password,
+      gethash: true
+    })
+    .pipe(
+      tap((data: any) => {
+        this.currentUser = data.user;
+        const token: string = data.token;
+        this.tokenService.addToken(token);
+        this.authenticated = true;
+        if (data.especialidad) {
+          this.router.navigate(['doctor/home']);
+        }
+        else { this.router.navigate(['paciente/home']); }
+      })
+    );
+  }
+
+  public logout(): void{
+    this.currentUser = null;
+    this.authenticated = false;
+    sessionStorage.removeItem('credentials');
+    this.router.navigate(['visitor/login']);
+  }
 }
