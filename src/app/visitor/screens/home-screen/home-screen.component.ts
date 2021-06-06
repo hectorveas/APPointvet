8e5214489658835be5b8fb32106a569eb306cc5b
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-06 01:46:20 -0400

    feat(visitor): new functions added

--- src/app/visitor/screens/home-screen/home-screen.component.ts
+++ src/app/visitor/screens/home-screen/home-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'visitor-home-screen',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-}
+  public goToSearch(): void{
+    this.router.navigate(['']);
+  };
+
+  public goToContact(): void{
+    this.router.navigate(['/visitor/contactanos']);
+  };
+};
