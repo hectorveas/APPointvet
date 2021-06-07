23de8af335c43a91cffab890a75b852a03c7b025
diego <diego.5alzamora@gmail.com>
2021-06-07 18:51:47 -0400

    feat(visitor): Se implementa el login

--- src/app/visitor/screens/login-screen/login-screen.component.ts
+++ src/app/visitor/screens/login-screen/login-screen.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, FormGroupDirective} from '@angular/forms';
+import { AuthService } from '@core/services/auth/auth.service';
+import { FormService } from '@core/services/form/form.service';
+import { NotificationService } from '../../../core/services/notification/notification.service';
+
 
 @Component({
   selector: 'visitor-login-screen',
@@ -7,9 +12,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginScreenComponent implements OnInit {
 
-  constructor() { }
+  public checkoutForm: FormGroup;
+
+  public passwordVisibility: boolean;
+
+
+  constructor(
+    private notificationService: NotificationService,
+    private formService: FormService,
+    private authService: AuthService,
+  ) { 
+    this.checkoutForm;
+    this.passwordVisibility = false;
+    this.createFormGroup();
+  }
 
   ngOnInit(): void {
+  }
+
+
+  private createFormGroup() {
+    this.checkoutForm = this.formService.buildFormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(3)]),
+    password: new FormControl('',[Validators.required]),
+  })
+}
+
+
+  public controlIsRequired(formControlName: string): boolean {
+    return this.formService.controlIsRequired(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalid(formControlName: string): boolean {
+    return this.formService.controlIsInvalid(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidEmail(formControlName: string): boolean {
+    return this.formService.controlIsInvalidEmail(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidPattern(formControlName: string): boolean {
+    return this.formService.controlIsInvalidPattern(this.checkoutForm, formControlName);
+  }
+
+  public controlIsInvalidLength(formControlName: string): boolean {
+    return this.formService.controlIsInvalidLength(this.checkoutForm, formControlName);
+  }
+
+  public async login(form: FormGroupDirective): Promise<void> {
+   
+    if (this.checkoutForm.valid) {
+      try {
+        await this.authService.login(this.checkoutForm.value).toPromise();
+      } catch (error) {
+        if (error.message === 'Access denied!') {
+          // this.notificationService.error('Tu cuenta no esta registrada como administrador');
+        } else {
+          // this.notificationService.error('Los datos ingresados son incorrectos');
+        }
+      }
+    }
+  }
+
+  personal(){}
+
+
+  usuario(){}
+
+
+  public togglePasswordVisibility(): void {
+    this.passwordVisibility = !this.passwordVisibility;
   }
 
 }
