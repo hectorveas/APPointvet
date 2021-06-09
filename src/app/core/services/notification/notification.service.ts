624e13a1d2f669498c89224f0d8925a443fcc8f4
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-08 20:22:27 -0400

    feat(services): notification service included

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ActiveToast, ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private toastrService: ToastrService) { }
+
+  public success(message: string): ActiveToast<any>{
+    return this.toastrService.success(message, 'Éxito!');
+  }
+
+  public warning(message: string): ActiveToast<any>{
+    return this.toastrService.warning(message, 'Alerta');
+  }
+
+  public info(message: string): ActiveToast<any>{
+    return this.toastrService.info(message);
+  }
+
+  public error(message: string): ActiveToast<any>{
+    return this.toastrService.error(message, 'Error');
+  }
+  
 }
