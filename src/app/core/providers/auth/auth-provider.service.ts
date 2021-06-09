95561786a109251d4ca40c15b8ae6dd20be75e1d
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-08 20:20:19 -0400

    feat(providers): authP newFunctions included&Fixed

--- src/app/core/providers/auth/auth-provider.service.ts
+++ src/app/core/providers/auth/auth-provider.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { PetOwner } from '@core/models/petOwner.models';
-import { Specialist } from '@core/models/specialist.models';
-import { HttpService } from '@core/services/http/http.service';
-import { TokenService } from '@core/services/token/token.service';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { HttpService } from '@core/services/http/http.service';
+import { TokenService } from '@core/services/token/token.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,44 +14,71 @@ export class AuthProviderService {
   private authenticated: boolean;
   private currentUser: any;
 
-  constructor(private httpService: HttpService, private tokenService: TokenService, private router: Router) {
+  constructor(
+    private httpService: HttpService,
+    private tokenService: TokenService,
+    private router: Router
+  ) {
     this.authenticated = false;
     this.currentUser = null;
 
-    if (sessionStorage.getItem('credentials')) {
+    if (sessionStorage.getItem('credentials')){
       this.authenticated = true;
     }
   }
 
-  public isAuthenticated(): boolean {
+  isAuthenticated(): boolean {
     return this.authenticated;
   }
 
-  public async getCurrentUser(): Promise<PetOwner | Specialist>{
-    if (!this.currentUser){
-      this.currentUser = await this.httpService.get<PetOwner | Specialist>('').toPromise();
-    }
+  getCurrentUser(): any {
     return this.currentUser;
   }
 
-  public login(email: string, password: string): Observable<any> {
-    const user = {
-      email: email,
-      password: password
-    };
-    return this.httpService.post<any>('/login', user)
-    .pipe(
-      tap((data: any) => {
-        this.currentUser = data.user;
-        const token: string = data.token;
-        this.tokenService.addToken(token);
-        this.authenticated = true;
-        if (data.especialidad) {
-          this.router.navigate(['doctor/home']);
-        }
-        else { this.router.navigate(['paciente/home']); }
+  login(auth: { email: string, password: string }, opcion: number): Observable<any> {
+    if (opcion == 1) {
+      return this.httpService.post('/auth/login/pet-owner', {
+        email: auth.email,
+        password: auth.password
       })
-    );
+        .pipe(
+          tap((data: any) => {
+            if ((data.user.role === 'petOwner')) {
+              const token: string = data.access_token;
+              this.currentUser = {
+                user: data.authenticated,
+                role: data.user.role
+              };
+              this.tokenService.addToken(token);
+              this.authenticated = true;
+              this.router.navigate(['paciente/citas']);
+            } else {
+              throw (new Error('Access denied!') && this.router.navigate(['visitor/inicio']));
+            }
+          })
+        );
+    } else {
+      return this.httpService.post('/auth/login/specialist', {
+        email: auth.email,
+        password: auth.password
+      })
+        .pipe(
+          tap((data: any) => {
+            if ((data.user.role === 'specialist')) {
+              const token: string = data.access_token;
+              this.currentUser = {
+                user: data.authenticated,
+                role: data.user.role
+              };
+              this.tokenService.addToken(token);
+              this.authenticated = true;
+                this.router.navigate(['doctor/citas']);
+            } else {
+              throw (new Error('Access denied!') && this.router.navigate(['visitor/inicio']));
+            }
+          })
+        );
+    }
   }
 
   public logout(): void{
@@ -61,4 +87,5 @@ export class AuthProviderService {
     sessionStorage.removeItem('credentials');
     this.router.navigate(['visitor/inicio']);
   }
+
 }
