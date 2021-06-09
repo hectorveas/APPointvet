2618475634b4b7820e78792c1a1424125f29192a
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-09 03:24:41 -0400

    chore(constants): list of specialities added

---
+++ src/app/core/constants/specialities.models.ts
@@ -0,0 +1,106 @@
+const Specialities = {
+  1: {
+    speciality: 'Veterinario ortopedista',
+    description: null
+  },
+  2: {
+    speciality: 'Veterinario cirujano',
+    description: null
+  },
+  3: {
+    speciality: 'Veterinario oncólogo',
+    description: null
+  },
+  4: {
+    speciality: 'Veterinario oftalmólogo',
+    description: null
+  },
+  5: {
+    speciality: 'Veterinario fisioterapeuta',
+    description: null
+  },
+  6: {
+    speciality: 'Veterinario dermatólogo',
+    description: null
+  },
+  7: {
+    speciality: 'Veterinario Exóticos',
+    description: null
+  },
+  8: {
+    speciality: 'Veterinario Equinos',
+    description: null
+  },
+  9: {
+    speciality: 'Veterinario Marino',
+    description: null
+  },
+  10: {
+    speciality: 'Veterinario Felinos salvajes',
+    description: null
+  },
+  11: {
+    speciality: 'Veterinario Bovinos',
+    description: null
+  },
+  12: {
+    speciality: 'Veterinario ganadero',
+    description: null
+  },
+  13: {
+    speciality: 'Veterinario obstetra',
+    description: null
+  },
+  14: {
+    specialiy: 'Veterinario anestesiologo',
+    description: null
+  },
+  15: {
+    speciality: 'Veterinario Comunitario',
+    description: 'No cobra'
+  },
+  16: {
+    speciality: 'Veterinario traumatologo',
+    description: null
+  },
+  17: {
+    speciality: 'Veterinario zootecnico',
+    description: null
+  },
+  18: {
+    speciality: 'Veterinario Rumiantes',
+    description: null
+  },
+  19: {
+    speciality: 'Veterinario Patologo',
+    description: null
+  },
+  20: {
+    speciality: 'Veterinario aviario',
+    description: null
+  },
+  21: {
+    speciality: 'Veterinario rapaces',
+    description: null
+  },
+  22: {
+    speciality: 'Veterinario farmacologico',
+    description: null
+  },
+  23: {
+    speciality: 'Veterinario fauna pequeña silvestre',
+    description: null
+  },
+  24: {
+    speciality: 'Veterinario fauna silvestre',
+    description: null
+  },
+  25: {
+    speciality: 'Veterinario fauna pesada silvestre',
+    description: null
+  },
+  26: {
+    speciality: 'Veterinario toxicologo',
+    description: 'Se encarga de animales marinos, anfibios venenosos, reptiles venenosos'
+  }
+};
