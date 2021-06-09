d3a616f4f0af87817c76e75bb914ce3f6fb82752
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-08 20:35:03 -0400

    chore(environments): environments included

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  PRIVATE_KEY: "DLjBsq0ZoytOpYwhA6SEzJ7XNMunCvd3",
+  apiBaseUrl: "https://glacial-cove-80501.herokuapp.com",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   PRIVATE_KEY: "DLjBsq0ZoytOpYwhA6SEzJ7XNMunCvd3",
-  apiBaseUrl: "https://glacial-cove-80501.herokuapp.com"
+  apiBaseUrl: "https://glacial-cove-80501.herokuapp.com",
+  apiBaseUrlDev: "http://localhost:3000"
 };
 
 /*
