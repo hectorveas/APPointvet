df680af3f4373dafc9c4518688485512fdc71cf5
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-09 20:53:22 -0400

    feat(providers): petOwner providers included

--- src/app/core/providers/patients/patient-provider.service.ts
+++ src/app/core/providers/patients/patient-provider.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { PetOwner } from '@core/models/petOwner.models';
+import { HttpService } from '@core/services/http/http.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientProviderService {
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService
+  ) { }
+
+  public getAllPacients(): Observable<PetOwner[]> {
+    return this.httpService.get<PetOwner[]>('/pet-owner');
+  };
+
+  public getPacientById(petOwnerId: string): Observable<PetOwner> {
+    return this.httpService.get<PetOwner>(`/pet-owner/${petOwnerId}`);
+  };
+
+  public getPacientByRut(petOwnerRut: string): Observable<PetOwner> {
+    return this.httpService.get<PetOwner>(`/pet-owner/search/${petOwnerRut}`);
+  };
+
+  public updatePacient(petOwnerId: string, body: any): Observable<Partial<PetOwner>> {
+    return this.httpService.put<PetOwner>(`/pet-owner/${petOwnerId}`, body);
+  };
+
+  public deletePacient(petOwnerId: string): Observable<PetOwner> {
+    return this.httpService.delete<PetOwner>(`/pet-owner/${petOwnerId}`);
+  };
+
+  public postPacient(body: any): Observable<PetOwner> {
+    return this.httpService.post<PetOwner>(`/pet-owner`, body);
+  };
 }
