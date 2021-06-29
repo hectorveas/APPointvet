3c3fa3feba8f912c8ed8618bc6a43ecd1c79eb60
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-06-29 17:02:50 -0400

    feat(visitor): search screen added

---
+++ src/app/visitor/screens/search-screen/search-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-screen',
+  templateUrl: './search-screen.component.html',
+  styleUrls: ['./search-screen.component.less']
+})
+export class SearchScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
